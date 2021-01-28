d802455656a94a042106560552fef8e42623f3b0
Denis <denislucasvalverdesilva@gmail.com>
2021-01-28 15:54:56 -0500

    Navbar 50%

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap 5 crashcourse</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body> 
+    <?php require "component/Header.html" ?>
+
+
+
+
+      
+    <script src="https://unpkg.com/@popperjs/core@2.4.0/dist/umd/popper.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
